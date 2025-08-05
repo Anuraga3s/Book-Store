bb429d7a382e95667165d7d7ee1e94fa9d9ff3e7
Anurag-cse <anurag14082003@gmail.com>
2025-08-05 22:07:23 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+import list from "../assets/list.json";
+
+import Cards from "./Cards";
+function Freebook() {
+ 
+  const filterData= list.filter((data)=>data.category==="Free");
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Books</h1>
+          <p>
+           All books on this website are carefully selected and offer great quality content to enhance your learning and reading experience.
+We’ve also included free demo versions of some books so you can explore before diving in.
+          </p>
+        </div>
+
+        <div>
+           <Slider {...settings}>
+        {filterData.map((item)=>(
+          <Cards item={item} key={item.id}/>
+        ))}  
+      </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Freebook;
