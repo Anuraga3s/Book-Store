5df9bf04603aae565ea6d8e89030a816c6e377b2
Anurag-cse <anurag14082003@gmail.com>
2025-11-26 00:51:37 +0530

    h

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Home from "./Home/Home";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Courses from "./Courses/courses";
+import Courses from "./components/Courses";
+
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuth } from "./context/AuthProvider";
